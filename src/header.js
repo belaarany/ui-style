672cdaae615607465c8edcb292f25c7eb9c36cca
Bela Arany <github@goabela.com>
2018-04-02 19:50:33 +0200

    Header has been extended

--- src/header.js
+++ src/header.js
@@ -1,12 +1,18 @@
 // Build stylesheet header
 
+let date = new Date().toLocaleString()
+let { version, author, license } = require("../package.json")
+
 module.exports = `\
 /*!
 * GOabela UI Style
 *
-* Author: Bela Arany
+* Author: ${author}
+* Version: ${version}
+* License: ${license}
 * Repository: https://github.com/belaarany/ui-style
-* Compiled: ${new Date().toLocaleString()}
+*
+* Compiled: ${date}
 */
 
 `
